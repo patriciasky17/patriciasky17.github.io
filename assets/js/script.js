a25b3c3b01f162583091bddf86edf25aecd7fdb2
Patricia Ho <patricia.ho@student.pradita.ac.id>
2021-10-14 20:12:49 +0700

    First Portofolio

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+function displayDate() {;
+  document.getElementById("waktu").innerHTML = Date();
+  }
+
+function formSent() {
+  alert("Your data has been sent! Please contact me via e-mail first : hopatricia1711@gmail.com");
+}
+
+function anotherFormSent() {
+  alert("Note : Please wait for 2 x 24 hours for response. Thank you :D");
+}
+
+function mouseOverSendMe(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 12 0px; text-align: center; ";
+  obj.innerHTML = "Send Me!"
+}
+
+function mouseOutSend(obj) {
+  obj.style.cssText="background: none; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:white; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 90px; text-align: center;";
+  obj.innerHTML = "Send!"
+}
+
+function mouseOverToAbout(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 170px; text-align: center; ";
+  obj.innerHTML = "Go to About";
+}
+
+function mouseOverToAchievement(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 190px; text-align: center;";
+  obj.innerHTML = "Go to Achievement";
+}
+
+function mouseOverToCollegeLife(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 190px; text-align: center";
+  obj.innerHTML = "Go to College Life";
+}
+
+function mouseOverToDailyLife(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 170px; text-align: center";
+  obj.innerHTML = "Go to Daily Life";
+}
+
+function mouseOverToSkill(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 170px; text-align: center";
+  obj.innerHTML = "Go to Skill";
+}
+
+function mouseOverToMyProject(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 190px; text-align: center";
+  obj.innerHTML = "Go to My Project"
+}
+
+function mouseOverToContactMe(obj) {
+  obj.style.cssText="background: white; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:black; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 190px; text-align: center";
+  obj.innerHTML = "Go to Contact Me"
+}
+
+function mouseOutHere(obj) {
+  obj.style.cssText="background: none; border-radius: 10px; border: 2px double white; padding: 15px 20px 15px 20px; color:white; cursor: pointer; font-weight: bold; text-decoration: none; transition-duration: 0.4s; width: 90px; text-align: center;";
+  obj.innerHTML = "Next";
+}
